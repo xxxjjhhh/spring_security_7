7461963c22148aa85940d9b6cb524a13ca9012ff
kimjihun <xxxjjhhh@naver.com>
2025-12-25 17:37:15 +0900

    * init

---
+++ src/main/java/com/yummi/seven/api/SampleController.java
@@ -0,0 +1,24 @@
+package com.yummi.seven.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+}
